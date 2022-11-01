92015db8cd9eb77f046c6c286932b787a3e2eb74
DmPilyak <metro200192@gmail.com>
2022-11-01 15:21:50 +0200

    v2

--- usercode.js
+++ usercode.js
@@ -1,3 +1,7 @@
 const Nexmo = require('nexmo');
 
-const jwt = Nexmo.generateJwt('private.key', {application_id: "5b48badd-aba5-47e9-95e0-c7bd94e8aea9"});
+module.exports = (data) => {
+  const jwt = Nexmo.generateJwt('private.key', {application_id: "5b48badd-aba5-47e9-95e0-c7bd94e8aea9"});
+
+  return jwt;
+};
